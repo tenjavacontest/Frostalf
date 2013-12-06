aa91275fb50accb05d2086f86d64ebb53744ea2f
frostalf <frostalf@gmail.com>
2013-12-06 14:21:09 -0800

    Initial Skeleton Setup

---
+++ src/main/java/net/frostalf/tenjava/TenJava.java
@@ -0,0 +1,13 @@
+package net.frostalf.tenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class TenJava 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
